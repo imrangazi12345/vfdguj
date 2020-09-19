fa146e524eb76b0d7f77101e9dee1cc586284890
imrangazi12345 <63030417+imrangazi12345@users.noreply.github.com>
2020-09-19 11:27:44 +0530

    Create ads-script.js

---
+++ ads-script.js
@@ -0,0 +1,201 @@
+
+        /*.   adsbox1.   */
+var adsbox1link1 ="";
+var adsbox1link2 ="";
+var adsbox1link3 ="";
+var adsbox1link4 ="";
+var adsbox1link5 ="";
+
+var adsbox1img1 ="'1.png'";
+var adsbox1img2 ="'2.png'";
+var adsbox1img3 ="'3.png'";
+var adsbox1img4 ="'4.png'";
+var adsbox1img5 ="'5.png'";
+
+      /*.   adsbox2.   */
+var adsbox2link1 ="";
+var adsbox2link2 ="";
+var adsbox2link3 ="";
+var adsbox2link4 ="";
+var adsbox2link5 ="";
+
+var adsbox2img1 ="'1.png'";
+var adsbox2img2 ="'2.png'";
+var adsbox2img3 ="'3.png'";
+var adsbox2img4 ="'4.png'";
+var adsbox2img5 ="'5.png'";
+
+        /*.   adsbox3.   */
+var adsbox3link1 ="";
+var adsbox3link2 ="";
+var adsbox3link3 ="";
+var adsbox3link4 ="";
+var adsbox3link5 ="";
+
+var adsbox3img1 ="'1.png'";
+var adsbox3img2 ="'2.png'";
+var adsbox3img3 ="'3.png'";
+var adsbox3img4 ="'4.png'";
+var adsbox3img5 ="'5.png'";
+
+      /*.   adsbox4.   */
+var adsbox4link1 ="";
+var adsbox4link2 ="";
+var adsbox4link3 ="";
+var adsbox4link4 ="";
+var adsbox4link5 ="";
+
+var adsbox4img1 ="'1.png'";
+var adsbox4img2 ="'2.png'";
+var adsbox4img3 ="'3.png'";
+var adsbox4img4 ="'4.png'";
+var adsbox4img5 ="'5.png'";
+
+      /*.   adsbox5.   */
+var adsbox5link1 ="";
+var adsbox5link2 ="";
+var adsbox5link3 ="";
+var adsbox5link4 ="";
+var adsbox5link5 ="";
+
+var adsbox5img1 ="'1.png'";
+var adsbox5img2 ="'2.png'";
+var adsbox5img3 ="'3.png'";
+var adsbox5img4 ="'4.png'";
+var adsbox5img5 ="'5.png'";
+
+
+  
+  
+  
+  
+  
+  /*   all codes hear eia par sersar mat karo ok 🔍   */
+  
+var adsbox1 = document.getElementById("ads-box1");
+var adsbox2 = document.getElementById("ads-box2");
+var adsbox3 = document.getElementById("ads-box3");
+var adsbox4 = document.getElementById("ads-box4");
+var adsbox5 = document.getElementById("ads-box5");
+
+      /*.   adsbox1.    */
+var adsbox1ads1 = '<a href="'+adsbox1link1+'" style="text-decoration:none;" ><div style="background:url(' + adsbox1img1 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox1ads2 = '<a href="'+adsbox1link2+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox1img2 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox1ads3 = '<a href="'+adsbox1link3+'" style="text-decoration:none;" ><div style="background:url(' + adsbox1img3 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox1ads4 = '<a href="'+adsbox1link4+'" style="text-decoration:none;" ><div style="background:url(' + adsbox1img4 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox1ads5 = '<a href="'+adsbox1link5+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox1img5 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+
+      /* adsbox2. */
+var adsbox2ads1 = '<a href="'+adsbox2link1+'" style="text-decoration:none;" ><div style="background:url(' + adsbox2img1 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox2ads2 = '<a href="'+adsbox2link2+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox2img2 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox2ads3 = '<a href="'+adsbox2link3+'" style="text-decoration:none;" ><div style="background:url(' + adsbox2img3 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox2ads4 = '<a href="'+adsbox2link4+'" style="text-decoration:none;" ><div style="background:url(' + adsbox2img4 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox2ads5 = '<a href="'+adsbox2link5+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox2img5 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+     
+     /*.   adsbox3.    */
+var adsbox3ads1 = '<a href="'+adsbox3link1+'" style="text-decoration:none;" ><div style="background:url(' + adsbox3img1 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox3ads2 = '<a href="'+adsbox3link2+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox3img2 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox3ads3 = '<a href="'+adsbox3link3+'" style="text-decoration:none;" ><div style="background:url(' + adsbox3img3 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox3ads4 = '<a href="'+adsbox3link4+'" style="text-decoration:none;" ><div style="background:url(' + adsbox3img4 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox3ads5 = '<a href="'+adsbox3link5+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox3img5 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+
+        /*.   adsbox4.    */
+var adsbox4ads1 = '<a href="'+adsbox4link1+'" style="text-decoration:none;" ><div style="background:url(' + adsbox4img1 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox4ads2 = '<a href="'+adsbox4link2+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox4img2 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox4ads3 = '<a href="'+adsbox4link3+'" style="text-decoration:none;" ><div style="background:url(' + adsbox4img3 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox4ads4 = '<a href="'+adsbox4link4+'" style="text-decoration:none;" ><div style="background:url(' + adsbox4img4 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox4ads5 = '<a href="'+adsbox4link5+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox4img5 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+
+        /*.   adsbox5.    */
+var adsbox5ads1 = '<a href="'+adsbox5link1+'" style="text-decoration:none;" ><div style="background:url(' + adsbox5img1 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox5ads2 = '<a href="'+adsbox5link2+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox5img2 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox5ads3 = '<a href="'+adsbox5link3+'" style="text-decoration:none;" ><div style="background:url(' + adsbox5img3 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox5ads4 = '<a href="'+adsbox5link4+'" style="text-decoration:none;" ><div style="background:url(' + adsbox5img4 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+var adsbox5ads5 = '<a href="'+adsbox5link5+'" style="text-decoration:none;" ><div  style="background:url(' + adsbox5img5 + ')no-repeat;"  class="ads-classSty" id="ads-box1"  style="ads-style" ><label class="ads-label" >Ad</label></div></a>';
+
+
+
+
+adsbox1.innerHTML=adsbox1ads4;
+adsbox2.innerHTML=adsbox2ads4;
+adsbox3.innerHTML=adsbox3ads4;
+adsbox4.innerHTML=adsbox4ads4;
+adsbox5.innerHTML=adsbox5ads4;
+
+var adsbox1DisplayAds = [adsbox1ads1,adsbox1ads2,adsbox1ads3,adsbox1ads4,adsbox1ads5];
+var adsbox2DisplayAds = [adsbox2ads1,adsbox2ads2,adsbox2ads3,adsbox2ads4,adsbox2ads5];
+var adsbox3DisplayAds = [adsbox3ads1,adsbox3ads2,adsbox3ads3,adsbox3ads4,adsbox3ads5];
+var adsbox4DisplayAds = [adsbox4ads1,adsbox4ads2,adsbox4ads3,adsbox4ads4,adsbox5ads5];
+var adsbox5DisplayAds = [adsbox5ads1,adsbox5ads2,adsbox5ads3,adsbox5ads4,adsbox5ads5];
+
+var adsbox1X = 0;
+var adsbox2X = 0;
+var adsbox3X = 0;
+var adsbox4X = 0;
+var adsbox5X = 0;
+
+setInterval(adsbox1Function, 15000);
+setInterval(adsbox2Function, 16000);
+setInterval(adsbox3Function, 15000);
+setInterval(adsbox4Function, 14000);
+setInterval(adsbox5Function, 17000);
+
+      /* adsbox1 */
+function adsbox1Function() {
+
+  if (adsbox1X < adsbox1DisplayAds.length){
+    adsbox1X = adsbox1X + 1;
+  }
+  else {
+    adsbox1X = 1;
+  }
+adsbox1.innerHTML = adsbox1DisplayAds[adsbox1X - 1];
+}
+
+      /* adsbox2 */
+function adsbox2Function() {
+
+  if (adsbox2X < adsbox2DisplayAds.length){
+    adsbox2X = adsbox2X + 1;
+  }
+  else {
+    adsbox2X = 1;
+  }
+adsbox2.innerHTML = adsbox2DisplayAds[adsbox2X - 1];
+}
+
+       /* adsbox3 */
+function adsbox3Function() {
+
+  if (adsbox3X < adsbox3DisplayAds.length){
+    adsbox3X = adsbox3X + 1;
+  }
+  else {
+    adsbox3X = 1;
+  }
+adsbox3.innerHTML = adsbox3DisplayAds[adsbox3X - 1];
+}
+
+        /* adsbox4 */
+function adsbox4Function() {
+
+  if (adsbox4X < adsbox4DisplayAds.length){
+    adsbox4X = adsbox4X + 1;
+  }
+  else {
+    adsbox4X = 1;
+  }
+adsbox4.innerHTML = adsbox4DisplayAds[adsbox4X - 1];
+}
+
+      /* adsbox5 */
+function adsbox5Function() {
+
+  if (adsbox5X < adsbox5DisplayAds.length){
+    adsbox5X = adsbox5X + 1;
+  }
+  else {
+    adsbox5X = 1;
+  }
+adsbox5.innerHTML = adsbox5DisplayAds[adsbox5X - 1];
+}
